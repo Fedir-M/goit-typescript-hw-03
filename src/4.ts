2e80624dc42edec3a254af925fc7c5ab196cc342
NormalOne <149543061+Fedir-M@users.noreply.github.com>
2024-03-19 00:15:07 -0400

    hw03, v1.0

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,70 @@
+//* class Key
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random(); //? а тут можна використати скорочену ініціалізацію як с таск№1
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+//* class Person
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+//* abstraction House
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[];
+
+  constructor(key: Key) {
+    this.door = false;
+    this.key = key;
+    this.tenants = [];
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`${person.getKey()} came in.`);
+    } else {
+      console.log("It is closed.");
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+//* class MyHouse
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(key: Key): void {
+    // Якщо ключ, переданий цьому методу, збігається з ключем, збереженим як key, то двері відчиняються.
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is open.");
+    } else {
+      console.log("Error: the wrong key.");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +73,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
